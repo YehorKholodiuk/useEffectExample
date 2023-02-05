170279076beef2804d8857b34ddebebd631c5b0c
ASUS <Yehor Kholodiuk>
2023-02-05 13:41:03 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import {useEffect, useState} from "react";
 
 function App() {
   const [resourceType, setResourceType] = useState('posts')
+    const [items, setItems] = useState([])
   useEffect (()=> {
     fetch (`https://jsonplaceholder.typicode.com/${resourceType}`)
         .then(response => response.json())
-        .then(json => console.log(json))
+        .then(json => setItems(json))
   }, [resourceType])
   return (
-    <div >
-     <button onClick ={() => setResourceType('posts')}>Posts</button>
-      <button onClick ={() => setResourceType('users')}>Users</button>
-      <button onClick ={() => setResourceType('comments')}>Comments</button>
-    </div>
+      <>
+          <div >
+              <button onClick ={() => setResourceType('posts')}>Posts</button>
+              <button onClick ={() => setResourceType('users')}>Users</button>
+              <button onClick ={() => setResourceType('comments')}>Comments</button>
+          </div>
+          <h1>{resourceType}</h1>
+          {items.map(item => {
+              return <pre>{JSON.stringify(item)}</pre>
+          })}
+
+      </>
+
+
   );
 }
 
